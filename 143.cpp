01dd6ae17bed7c04ec35fa0730807522bcd6f4b6
wellexam <el.zhao.me@gmail.com>
2021-09-01 18:54:57 +0800

    nt

---
+++ 143.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void reorderList(ListNode *head)
+{
+    vector< ListNode * > list;
+    list.reserve(50000);
+    auto iter = head;
+    while (iter)
+    {
+        list.push_back(iter);
+        iter = iter->next;
+    }
+    int len = list.size();
+    int back = len - 1;
+    int i = 0;
+    for (; i < back - i; ++i)
+    {
+        list[i]->next = list[back - i];
+        list[back - i]->next = list[i + 1];
+    }
+    list[i]->next = nullptr;
+}
+
+int main()
+{
+    auto *head = new ListNode(0);
+    auto heads = head;
+    head->next = new ListNode(1);
+    head = head->next;
+    head->next = new ListNode(2);
+    head = head->next;
+    head->next = new ListNode(3);
+    head = head->next;
+    head->next = new ListNode(4);
+    head = head->next;
+    head->next = new ListNode(5);
+    head = head->next;
+    head->next = new ListNode(6);
+    head = head->next;
+    //head->next = new ListNode(7);
+    //head = head->next;
+    reorderList(heads);
+    return 0;
+}
---
+++ 25.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* reverseKGroup(ListNode* head, int k) {
+    auto *dummy_node = new ListNode(0, head);
+    auto iter = head, temp = head->next, last = dummy_node;
+    while (iter)
+    {
+        int count = 0;
+        temp = iter;
+        while (temp && count <= k - 1)
+        {
+            temp = temp->next;
+            ++count;
+        }
+        if (count <= k - 1)
+            break;
+        last->next = temp;
+        temp = temp->next;
+        for (int i = 0; i < k; ++i)
+        {
+            auto temp_s = iter->next;
+            iter->next = temp;
+            temp = iter;
+            iter = temp_s;
+        }
+        last = iter;
+        iter = iter->next;
+    }
+    return dummy_node->next;
+}
