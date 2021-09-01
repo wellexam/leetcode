406bfa3ef3367cdc2e707ce661c01feddfcbebac
wellexam <el.zhao.me@gmail.com>
2021-09-01 21:18:58 +0800

    nt

--- 25.cpp
+++ 25.cpp
@@ -19,18 +19,20 @@ struct ListNode
 };
 
 ListNode* reverseKGroup(ListNode* head, int k) {
+	if (k == 1)
+		return head;
     auto *dummy_node = new ListNode(0, head);
     auto iter = head, temp = head->next, last = dummy_node;
     while (iter)
     {
         int count = 0;
         temp = iter;
-        while (temp && count <= k - 1)
+        while (temp && count < k - 1)
         {
             temp = temp->next;
             ++count;
         }
-        if (count <= k - 1)
+        if (count < k - 1 || !temp)
             break;
         last->next = temp;
         temp = temp->next;
@@ -41,8 +43,24 @@ ListNode* reverseKGroup(ListNode* head, int k) {
             temp = iter;
             iter = temp_s;
         }
-        last = iter;
-        iter = iter->next;
+        last = temp->next;
     }
     return dummy_node->next;
+}
+
+int main()
+{
+    auto *head = new ListNode(1);
+    auto record = head;
+    head->next = new ListNode(2);
+    head = head->next;
+    head->next = new ListNode(3);
+    head = head->next;
+    head->next = new ListNode(4);
+    head = head->next;
+    head->next = new ListNode(5);
+	head = head->next;
+	head->next = new ListNode(6);
+    record = reverseKGroup(record, 3);
+    return 0;
 }
