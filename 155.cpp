0e9713b8352ecdb3f685fe1266682a3081179d83
wellexam <el.zhao.me@gmail.com>
2021-09-02 10:53:42 +0800

    nt

---
+++ 155.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class MinStack
+{
+private:
+	vector<int> stack;
+	vector<int> min_stack;
+	int min;
+	int top_pos = -1;
+
+public:
+	/** initialize your data structure here. */
+	MinStack()
+	{
+		min = 0x7fffffff;
+	}
+
+	void push(int val)
+	{
+		stack.push_back(val);
+		++top_pos;
+		if (val < min)
+			min = val;
+		min_stack.push_back(min);
+	}
+
+	void pop()
+	{
+		stack.erase(stack.end() - 1);
+		min_stack.erase(min_stack.end() -1);
+		--top_pos;
+		if (top_pos < 0)
+			min = 0x7fffffff;
+		else
+			min = min_stack[top_pos];
+	}
+
+	int top()
+	{
+		return stack[top_pos];
+	}
+
+	int getMin()
+	{
+		return min_stack[top_pos];
+	}
+};
+
+int main()
+{
+	MinStack* obj = new MinStack();
+	obj->push(-2);
+	obj->push(0);
+	obj->push(-3);
+	int a = obj->getMin();
+	obj->pop();
+	a = obj->top();
+	a = obj->getMin();
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
---
+++ 1823.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <stack>
+
+using namespace std;
+
+int findTheWinner(int n, int k) {
+
+}
