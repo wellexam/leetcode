fbbdc409ac6e3a7a502be1b872daf441e84b542a
wellexam <el.zhao.me@gmail.com>
2021-09-24 22:07:42 +0800

    nt

--- 213.cpp
+++ 213.cpp
@@ -2,24 +2,25 @@
 
 using namespace std;
 
-int money_even(vector<int> &nums){
-    auto sum_1 = 0, sum_2 = 0;
-    for (int i = 0; i < nums.size(); i += 2) {
-        sum_1 += nums[i];
-    }
-    for (int i = 1; i < nums.size(); i += 2) {
-        sum_2 += nums[i];
-    }
-    return max(sum_1, sum_2);
-}
-
-int money_odd(vector<int> &nums) {}
-
 int rob(vector<int> &nums) {
     int n = nums.size();
-    if (!n % 2) {
-        return money_even(nums);
-    } else {
-        return money_odd(nums);
+    if (n == 1) {
+        return nums[0];
     }
+    if (n == 2) {
+        return max(nums[0], nums[1]);
+    }
+    vector<int> dp(n, 0);
+    dp[0] = nums[0];
+    dp[1] = max(nums[0], nums[1]);
+    for (int i = 2; i < n - 1; ++i) {
+        dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
+    }
+    auto max = dp[n - 2];
+    dp[1] = nums[1];
+    dp[2] = std::max(nums[1], nums[2]);
+    for (int i = 3; i < n; ++i) {
+        dp[i] = std::max(dp[i - 1], nums[i] + dp[i - 2]);
+    }
+    return std::max(max, dp[n - 1]);
 }
