8b9127d9b41451f917fd1167b403ec3a96c37376
wellexam <el.zhao.me@gmail.com>
2021-12-01 19:58:21 +0800

    nt

--- 22.cpp
+++ 22.cpp
@@ -2,10 +2,57 @@
 
 using namespace std;
 
-vector<vector<string>> table;
-vector<vector<int>> num_table;
-map<int, bool> flag;
+map<int, vector<string>> str_table;
 
-void generator(int n, vector<int> nums) {}
+void str_generator(int n, int target, string str) {
+    if (!n) {
+        str += ")";
+        str_table[target].push_back(str);
+        return;
+    }
+    if (n != target) {
+        for (int i = 1; i <= n; ++i) {
+            for (auto &s : str_table[i]) {
+                str_generator(n - i, target, str + s);
+            }
+        }
+    }
+    if (n == target) {
+        str_generator(n - 1, target, "(");
+    }
+}
 
-vector<string> generateParenthesis(int n) {}
+void generator(int n, int target, const string& str) {
+    if (n <= 1) {
+        if (n == 1) {
+            str_table[target].push_back(str + "()");
+        } else {
+            str_table[target].push_back(str);
+        }
+        return;
+    } else {
+        for (int i = 1; i <= n && i < target; ++i) {
+            for (auto &s : str_table[i]) {
+                generator(n - i, target, str + s);
+            }
+        }
+    }
+}
+
+vector<string> generateParenthesis(int n) {
+    if (n == 1) {
+        return {"()"};
+    }
+    str_table[1].push_back({"()"});
+    for (int i = 2; i <= n; ++i) {
+        str_generator(i, i, "");
+    }
+    generator(n, n, "");
+    return str_table[n];
+}
+
+int main() {
+    for (auto &i : generateParenthesis(1)) {
+        cout << i << endl;
+    }
+}
