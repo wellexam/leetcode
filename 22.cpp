4757b04d522519263957fccb25d20d23b4a6a93d
wellexam <el.zhao.me@gmail.com>
2021-12-01 16:00:23 +0800

    nt

--- 22.cpp
+++ 22.cpp
@@ -2,40 +2,10 @@
 
 using namespace std;
 
-// void dfs(int count, string str,int closing_count, vector<string> &ans) {
-//     if (!count) {
-//         while (closing_count) {
-//             --closing_count;
-//             str.push_back('c');
-//         }
-//         ans.emplace_back(str);
-//         return;
-//     }
-//     while (count) {
-//         --count;
-//         str.push_back('(');
-//         ++closing_count;
-//         dfs(count, str, closing_count, ans);
-//     }
-// }
+vector<vector<string>> table;
+vector<vector<int>> num_table;
+map<int, bool> flag;
 
-vector<string> generateParenthesis(int n) {
-    vector<string> last{"()"};
-    if (n == 1) {
-        return last;
-    }
-    --n;
-    while (n) {
-        vector<string> current;
-        for (auto &i : last) {
-            current.emplace_back("(" + i + ")");
-            current.emplace_back("()" + i);
-            current.emplace_back(i + "()");
-        }
-        current.pop_back();
-        --n;
-        last = current;
-    }
-    sort(last.begin(), last.end());
-    return last;
-}
+void generator(int n, vector<int> nums) {}
+
+vector<string> generateParenthesis(int n) {}
