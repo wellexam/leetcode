d6435637dbc0e3c50c17b71603812155f5fc2237
wellexam <el.zhao.me@gmail.com>
2021-12-09 23:27:40 +0800

    nt

--- 470.cpp
+++ 470.cpp
@@ -5,5 +5,13 @@ using namespace std;
 int rand7();
 
 int rand10() {
-    
+    int a = rand7(), b = rand7();
+    while (a > 5) {
+        a = rand7();
+    }
+    while (b > 6) {
+        b = rand7();
+    }
+    b %= 2;
+    return a + b * 5;
 }
