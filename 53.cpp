9bef90285e89830bcc52079c137b43a41f306d84
wellexam <el.zhao.me@gmail.com>
2022-03-21 21:00:52 +0800

    nt

--- 53.cpp
+++ 53.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 int maxSubArray(vector<int> &nums) {
-    auto size = nums.size();
-    int ans = nums[0], min = nums[0];
-    for (int i = 1; i < size; ++i) {
-        nums[i] = nums[i - 1] + nums[i];
-        if (min > nums[i - 1]) {
-            min = nums[i - 1];
-        }
-        if (min >= 0) {
-            ans = ans > nums[i] ? ans : nums[i];
-        } else {
-            ans = ans > nums[i] - min ? ans : nums[i] - min;
-        }
+    int min_sum = 0, ans = nums[0];
+    int sum = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        sum += nums[i];
+        ans = max(ans, sum - min_sum);
+        min_sum = min(min_sum, sum);
     }
     return ans;
+}
+
+int main() {
+    vector<int> nums{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    maxSubArray(nums);
+    return 0;
 }
