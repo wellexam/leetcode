0346fc140970ff009c4b88635727b05d848246ac
wellexam <el.zhao.me@gmail.com>
2022-03-30 19:49:01 +0800

    nt

--- 460.cpp
+++ 460.cpp
@@ -1,126 +1,122 @@
-#include <utility>
-
 #include "regular_headers.hpp"
 
 using namespace std;
 
 class LFUCache {
-    class weight {
+public:
+    class key_value {
     public:
-        int count = 0;
-        list<int> que;
-
-        weight(int _count, int key) : count(_count), que({key}) {}
+        int key;
+        int value;
+        int freq;
     };
 
-    class position {
+    class list_iter {
     public:
-        int value = 0;
-        list<weight>::iterator iter_1;
-        list<int>::iterator iter_2;
-
-        position() = default;
-        position(int _value, list<weight>::iterator _iter_1, list<int>::iterator _iter_2) : value(_value), iter_1(_iter_1), iter_2(_iter_2) {}
+        list<list<key_value>>::iterator iter_1;
+        list<key_value>::iterator iter_2;
     };
 
-    unordered_map<int, position> cache_map;
-    list<weight> frequency;
+    unordered_map<int, list_iter> hash;
+    list<list<key_value>> frequency;
     int cap;
 
-    void func(int key, int value) {
-        frequency.emplace_back(1, key);
-        auto iter_1 = --frequency.end();
-        auto iter_2 = iter_1->que.begin();
-        cache_map[key] = position(value, iter_1, iter_2);
-    }
-
-    void put_impl(int key, int value) {
-        if (frequency.empty()) {
-            func(key, value);
-        } else {
-            if (frequency.back().count == 1) {
-                frequency.back().que.push_front(key);
-                cache_map[key] = position(value, --frequency.end(), frequency.back().que.begin());
-            } else {
-                func(key, value);
-            }
-        }
-    }
-
-    void move_key(int key) {
-        auto &ref = cache_map[key];
-        auto iter_1 = ref.iter_1;
-        auto iter_2 = ref.iter_2;
-        if (iter_1->count == frequency.begin()->count) {
-            frequency.emplace_front(iter_1->count + 1, key);
-            iter_1->que.erase(iter_2);
-            if (iter_1->que.empty()) {
-                frequency.erase(iter_1);
-            }
-            ref.iter_1 = frequency.begin();
-            ref.iter_2 = ref.iter_1->que.begin();
-        } else {
-            auto prev_iter = iter_1;
-            --prev_iter;
-            if (prev_iter->count == iter_1->count + 1) {
-                prev_iter->que.push_front(key);
-                iter_1->que.erase(iter_2);
-                if (iter_1->que.empty()) {
-                    frequency.erase(iter_1);
-                }
-                ref.iter_1 = prev_iter;
-                ref.iter_2 = prev_iter->que.begin();
-            } else {
-                ref.iter_1 = frequency.emplace(iter_1, iter_1->count + 1, key);
-                ref.iter_2 = ref.iter_1->que.begin();
-                iter_1->que.erase(iter_2);
-                if (iter_1->que.empty()) {
-                    frequency.erase(iter_1);
-                }
-            }
-        }
-    }
-
-public:
-    LFUCache(int capacity) : cap(capacity) { cache_map.reserve(cap); }
+    LFUCache(int capacity) { cap = capacity; }
 
     int get(int key) {
-        auto res = cache_map.find(key);
-        if (res != cache_map.end()) {
-            move_key(key);
-            return cache_map[key].value;
-        } else {
+        auto pos = hash.find(key);
+        if (pos == hash.end()) {
             return -1;
         }
+        auto iter = pos->second;
+        int ret = iter.iter_2->value;
+        if (iter.iter_1 == frequency.begin()) {
+            frequency.push_front({});
+            frequency.front().splice(frequency.front().end(), *iter.iter_1, iter.iter_2);
+            iter.iter_1--;
+            iter.iter_2->freq++;
+        } else {
+            auto ahead_iter = iter.iter_1;
+            ahead_iter--;
+            if (ahead_iter->begin()->freq == iter.iter_2->freq + 1) {
+                ahead_iter->splice(ahead_iter->end(), *iter.iter_1, iter.iter_2);
+                iter.iter_1--;
+                iter.iter_2->freq++;
+            } else {
+                frequency.insert(iter.iter_1, {});
+                ahead_iter = iter.iter_1;
+                ahead_iter--;
+                ahead_iter->splice(ahead_iter->end(), *iter.iter_1, iter.iter_2);
+                iter.iter_1--;
+                iter.iter_2->freq++;
+            }
+        }
+        return ret;
     }
 
     void put(int key, int value) {
         if (cap == 0) {
             return;
         }
-        auto res = cache_map.find(key);
-        if (res == cache_map.end()) {
-            if (cache_map.size() != cap) {
-                put_impl(key, value);
+        auto pos = hash.find(key);
+        if (pos != hash.end()) {
+            auto iter = pos->second;
+            if (iter.iter_1 == frequency.begin()) {
+                frequency.push_front({});
+                frequency.front().splice(frequency.front().end(), *iter.iter_1, iter.iter_2);
+                iter.iter_1--;
+                iter.iter_2->freq++;
             } else {
-                auto to_delete = frequency.back().que.back();
-                cache_map.erase(to_delete);
-                frequency.back().que.pop_back();
-                if (frequency.back().que.empty()) {
-                    frequency.pop_back();
+                auto ahead_iter = iter.iter_1;
+                ahead_iter--;
+                if (ahead_iter->begin()->freq == iter.iter_2->freq + 1) {
+                    ahead_iter->splice(ahead_iter->end(), *iter.iter_1, iter.iter_2);
+                    iter.iter_1--;
+                    iter.iter_2->freq++;
+                } else {
+                    frequency.insert(iter.iter_1, {});
+                    ahead_iter = iter.iter_1;
+                    ahead_iter--;
+                    ahead_iter->splice(ahead_iter->end(), *iter.iter_1, iter.iter_2);
+                    iter.iter_1--;
+                    iter.iter_2->freq++;
                 }
-                put_impl(key, value);
             }
-        } else {
-            cache_map[key].value = value;
-            move_key(key);
+            return;
         }
+        if (hash.size() == cap) {
+            auto tail = frequency.back();
+            hash.erase(tail.front().key);
+            tail.pop_front();
+            if (tail.empty()) {
+                frequency.pop_back();
+            }
+        }
+        key_value temp;
+        temp.key = key;
+        temp.value = value;
+        temp.freq = 1;
+        if (frequency.empty()) {
+            frequency.push_back({temp});
+        } else if (frequency.back().front().freq == 1) {
+            frequency.back().push_back(temp);
+        } else {
+            frequency.push_back({temp});
+        }
+        list_iter iters;
+        iters.iter_1 = frequency.end();
+        iters.iter_1--;
+        iters.iter_2 = iters.iter_1->end();
+        iters.iter_2--;
+        hash[key] = iters;
+        return;
     }
 };
 
 void test_func(vector<int> &param, LFUCache &s) {
     if (param.size() == 1) {
-        auto sb = s.get(param[0]);
+        auto temp = param[0];
+        auto sb = s.get(temp);
         cout << sb << " ";
         if (sb == 18) {
             sb = 0;
@@ -129,7 +125,9 @@ void test_func(vector<int> &param, LFUCache &s) {
         if (param[1] == 23) {
             param[1] = 23;
         }
-        s.put(param[0], param[1]);
+        auto temp1 = param[0];
+        auto temp2 = param[1];
+        s.put(temp1, temp2);
         cout << "null ";
     }
 }
@@ -137,13 +135,7 @@ void test_func(vector<int> &param, LFUCache &s) {
 int main() {
     vector<int> ans;
     LFUCache s(10);
-    vector<vector<int>> test{{10, 13}, {3, 17}, {6, 11}, {10, 5},  {9, 10}, {13},     {2, 19}, {2},     {3},      {5, 25},  {8},      {9, 22},  {5, 5},   {1, 30}, {11},     {9, 12},
-                             {7},      {5},     {8},     {9},      {4, 30}, {9, 3},   {9},     {10},    {10},     {6, 14},  {3, 1},   {3},      {10, 11}, {8},     {2, 14},  {1},
-                             {5},      {4},     {11, 4}, {12, 24}, {5, 18}, {13},     {7, 23}, {8},     {12},     {3, 27},  {2, 12},  {5},      {2, 9},   {13, 4}, {8, 18},  {1, 7},
-                             {6},      {9, 29}, {8, 21}, {5},      {6, 30}, {1, 12},  {10},    {4, 15}, {7, 22},  {11, 26}, {8, 17},  {9, 29},  {5},      {3, 4},  {11, 30}, {12},
-                             {4, 29},  {3},     {9},     {6},      {3, 4},  {1},      {10},    {3, 29}, {10, 28}, {1, 20},  {11, 13}, {3},      {3, 12},  {3, 8},  {10, 9},  {3, 26},
-                             {8},      {7},     {5},     {13, 17}, {2, 27}, {11, 15}, {12},    {9, 19}, {2, 15},  {3, 16},  {1},      {12, 17}, {9, 1},   {6, 19}, {4},      {5},
-                             {5},      {8, 1},  {11, 7}, {5, 2},   {9, 28}, {1},      {2, 2},  {7, 4},  {4, 22},  {7, 24},  {9, 26},  {13, 28}, {11, 26}};
+    vector<vector<int>> test{{2}, {1, 1}, {2, 2}, {1}, {3, 3}, {2}, {3}, {4, 4}, {1}, {3}, {4}};
     for (auto &i : test) {
         test_func(i, s);
     }
--- 887.cpp
+++ 887.cpp
@@ -2,52 +2,34 @@
 
 using namespace std;
 
-vector<vector<int>> table;
-
-void find_mid(int i, int j) {
-    int begin = 1, end = j, mid = (begin + end) >> 1;
-    while (begin < end) {
-        mid = (begin + end) >> 1;
-        if (table[i][j - mid] > table[i - 1][mid - 1]) {
-            if (begin != mid) {
-                begin = mid;
-                continue;
-            } else {
-                break;
-            }
-        }
-        if (table[i][j - mid] < table[i - 1][mid - 1]) {
-            if (end != mid) {
-                end = mid;
-                continue;
-            } else {
-                break;
-            }
-        }
-        if (table[i][j - mid] == table[i - 1][mid - 1]) {
-            table[i][j] = table[i][j - mid] + 1;
-            return;
-        }
-    }
-    table[i][j] = min(table[i][j - mid], table[i - 1][mid]) + 1;
-}
-
 int superEggDrop(int k, int n) {
-    table = vector<vector<int>>(k + 1, vector<int>(n + 1, 0));
-    for (int i = 1; i <= k; ++i) {
-        table[i][1] = 1;
+    vector<vector<int>> dp(k + 1, vector<int>(n + 1, 0));
+    for (int i = 1; i <= n; i++) {
+        dp[1][i] = i;
     }
-    for (int i = 1; i <= n; ++i) {
-        table[1][i] = i;
+    for (int i = 1; i <= k; i++) {
+        dp[i][1] = 1;
     }
-    for (int i = 2; i <= k; ++i) {
-        for (int j = 2; j <= n; ++j) {
-            find_mid(i, j);
+    for (int i = 2; i <= k; i++) {
+        for (int j = 2; j <= n; j++) {
+            int value = n;
+            for (int x = 1; x <= j; x++) {
+                value = min(value, max(dp[i - 1][x - 1], dp[i][j - x]));
+            }
+            dp[i][j] = value + 1;
         }
     }
-    return table[k][n];
+    for (auto &i : dp) {
+        for (auto &j : i) {
+            cout << j << ' ';
+            if (j < 10)
+                cout << ' ';
+        }
+        cout << endl;
+    }
+    return dp[k][n];
 }
 
 int main() {
-    cout << superEggDrop(6, 1000000);
+    cout << superEggDrop(4, 35);
 }
